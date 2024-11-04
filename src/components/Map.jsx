8ac4fdf388c0c9af088ab6738212cfda59b7fd63
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-11-04 17:17:18 +0600

    first commit

---
+++ src/components/Map.jsx
@@ -0,0 +1,18 @@
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
+const Map = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        Position: {lat} {lng}
+      </h2>
+    </div>
+  );
+};
+export default Map;
