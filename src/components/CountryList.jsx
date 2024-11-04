0d6025a0fbc3a341cde2bcb5c637fa90555310f9
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-11-04 17:22:41 +0600

    removed key prop from countryList

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -24,7 +24,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} key={country.id} />
+        <CountryItem key={cities.id} />
       ))}
     </ul>
   );
